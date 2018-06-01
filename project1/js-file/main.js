ceb8664b10f431c9f2d28759305f5d646bfb6d3d
Noon <noon@Noons-MacBook-Pro.local>
2018-05-31 20:11:00 -0400

    to loop through input array and make it to uppercase

--- project1/js-file/main.js
+++ project1/js-file/main.js
@@ -11,24 +11,38 @@ var blankSpace = document.querySelector('.blank');
 setWord.addEventListener('submit', function(e){
 	e.preventDefault();
 		
+
 		// to split the input from user to make it to an array
 		content = input.value.split("");
+
+		//to loop through each in array and make it all to uppercase
+		//to prevent the problem when the user enters mix uppercase and lowercase
+		for(var i = 0; i < content.length; i++){
+			content[i] = content[i].toUpperCase();
+		}
+
+		//content.forEach(currentContent => currentContent = currentContent.toUpperCase());
 		
-		console.log(`content lenght ${content.length}`);
+		console.log(`content array uppercase ${content}`)
+		
+		
 
 	//if user enters any letter, it will create no. of divs according to the content.lenght
 	//after user hit enter, form will be hidden
 	if(content.length > 0){
 		document.querySelector('form').style.visibility = 'hidden';
-
 		for(var i = 0; i < content.length; i++){
 			var div = document.createElement('div');
 			div.classList.add('underline');
 			blankSpace.appendChild(div);
 			var span = document.createElement('span')	
-			div.appendChild(span); // add a span to 
+			div.appendChild(span); // add a span to a div
+			// make the input array to display to uppercase 
+			//console.log(`uppercase input ${content[i].toUpperCase()}`);
+			console.log(content[i].toUpperCase());
 			span.innerText = content[i];
-
+			
+			
 
 			div.style.display = "block";
 		}
@@ -51,7 +65,8 @@ setWord.addEventListener('submit', function(e){
 		e.preventDefault();
 
 			// get the value of each alphabet when user clicks and make it all to lowercase for comparison
-			var alphabet = e.target.innerText.toLowerCase();
+			// var alphabet = e.target.innerText.toLowerCase();
+			var alphabet = e.target.innerText;
 			var span = document.querySelectorAll('span');
 
 			
